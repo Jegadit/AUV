4c2427b19c9454eca1ced9627886b6322acdc3ec
rulerofeternalnight <fakeidnor@gmail.com>
2023-07-07 14:44:29 +0530

    commented boto db

--- app.py
+++ app.py
@@ -12,35 +12,35 @@ from boto3.dynamodb.conditions import Key, Attr
 ############################################### AWS# ###################################################
 
 
-my_config = Config(
-    region_name = 'us-east-1'
-)
+# my_config = Config(
+#     region_name = 'us-east-1'
+# )
 
-session = boto3.session.Session(
-    aws_access_key_id='####',
-    aws_secret_access_key='###',
-    aws_session_token='###'
-)
+# session = boto3.session.Session(
+#     aws_access_key_id='ASIA4XEFNQPVAHAWL6WQ',
+#     aws_secret_access_key='3rB/yS/oOOr/s7Dda2+t6wJH/+4pstSG73xzdrSx',
+#     aws_session_token='FwoGZXIvYXdzEKz//////////wEaDE0+MkfHCoKx8sfyECLEAUFwD6Lox+s5TrLN1PrNAv88dIRziTz8Ks/L7yNJVCSVer2olwMLV5EfgU25NF7kXJRUwhEzh1QWNxjTaVyc8y2TyrJ6zdQL+xLX2BKXnXrtCzd5dgYzQB03LhuEDEOsXtj4ROldbDuaoVv5Cd9q+eVaSerwlvPw9t6crqoBmiBKtXB7wnbUPLOpBvwY5DeV3WQdEHykvw9Lg15xHyVZhEXPkwsuWnk2QlDqTx/8owJ6ht1ex5/TzyLyP/AYnXDak18+MxooiP7mowYyLQS80UqGjaW05/N/fDsxKp/DqmagXFQtyJUg459SgNDsRVDIRULLmXtTt4wlcQ=='
+# )
 
-__TableName__ = 'DWMC_ESP32_BMP280_table'
+# __TableName__ = 'DWMC_ESP32_BMP280_table'
 
-client = session.client('dynamodb', config=my_config)
-DB = session.resource('dynamodb', config=my_config)
+# client = session.client('dynamodb', config=my_config)
+# DB = session.resource('dynamodb', config=my_config)
 
-table = DB.Table(__TableName__)
+# table = DB.Table(__TableName__)
 
 
-def getdata():
-    response = table.scan( 
-        FilterExpression = Attr('timestamp').ne(0)
-    )
-    return response
+# def getdata():
+#     response = table.scan( 
+#         FilterExpression = Attr('timestamp').ne(0)
+#     )
+#     return response
 
 
 ############################################### ESP ####################################################
 
 
-esp_root_url = "http://192.168.73.240"
+esp_root_url = "http://192.168.42.240"
 
 def sendRequest(url):
     try:
@@ -147,6 +147,12 @@ def control():
             elif 's' in request.form.to_dict()['direction']:
                 sendRequest(esp_root_url + "/s")
                 print("Stop")
+            elif 'a' in request.form.to_dict()['direction']:
+                sendRequest(esp_root_url + "/a")
+                print("Stop")
+            elif 'd' in request.form.to_dict()['direction']:
+                sendRequest(esp_root_url + "/d")
+                print("Stop")
 
         return render_template('arrow.html')
     else:
@@ -175,7 +181,7 @@ def user():
 def iot():
     if 'user' in FlaskSession:
         user = FlaskSession['user']
-        response = getdata()
+        response = [] #getdata()
         return render_template('table.html', data = response["Items"], sizeofresult = response["Items"].__len__())
     else:
         return redirect(url_for("login"))
@@ -188,7 +194,7 @@ def status():
 
 @app.route("/live")
 def live():
-    return redirect('http://192.168.137.36:8080/')
+    return redirect('http://192.168.42.207:8080/')
 
 
 @app.route("/logout")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+
+
+
+############################################### AWS# ###################################################
+
+
+my_config = Config(
+    region_name = 'us-east-1'
+)
+
+session = boto3.session.Session(
+    aws_access_key_id='ASIA4XEFNQPVAHAWL6WQ',
+    aws_secret_access_key='3rB/yS/oOOr/s7Dda2+t6wJH/+4pstSG73xzdrSx',
