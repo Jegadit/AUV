725dd1fd1c9685d08591147ce8d5d10e27e73491
rulerofeternalnight <fakeidnor@gmail.com>
2023-09-21 13:06:18 +0530

    docs folder

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-  region_name = 'us-east-1'
-# )
-
-# session = boto3.session.Session(
-#     aws_access_key_id='ASIA4XEFNQPVAHAWL6WQ',
-#     aws_secret_access_key='3rB/yS/oOOr/s7Dda2+t6wJH/+4pstSG73xzdrSx',
-#     aws_session_token
